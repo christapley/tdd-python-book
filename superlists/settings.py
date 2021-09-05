3c260ca21059c8975ab5ab34f821e33bb8261077
Chris Tapley <chris.tapley.81@gmail.com>
2021-09-05 20:46:09 +0800

    decouple

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(9qz)p)1@x6b@66*cmdugebc(mwcnvs(t357e(n9inh&$c%@8x'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
